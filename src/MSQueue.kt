2997a24d91105c4d2a40ee2fd3af06e877bb918a
alexeyha <piratlex2003@mail.ru>
2023-09-16 20:19:38 +0200

    Implement MSQueue according to the article

--- src/MSQueue.kt
+++ src/MSQueue.kt
@@ -4,12 +4,51 @@ class MSQueue<E> : Queue<E> {
     private val head: AtomicReference<Node<E>>
     private val tail: AtomicReference<Node<E>>
 
+    init {
+        val firstNode = Node<E>(null)
+        head = AtomicReference(firstNode)
+        tail = AtomicReference(firstNode)
+    }
+
     override fun enqueue(element: E) {
-        TODO("Implement me")
+        val node: Node<E> = Node(element)
+        var curTail : Node<E>
+        while (true) {
+            curTail = tail.get()
+            val next = curTail.next.get()
+            if (curTail === tail.get()) {
+                if (next == null) {
+                    if (curTail.next.compareAndSet(null, node)) {
+                        break
+                    }
+                }
+                else tail.compareAndSet(curTail, next)
+            }
+        }
+        tail.compareAndSet(curTail, node)
     }
 
     override fun dequeue(): E? {
-        TODO("implement me")
+        var element: E?
+        while (true) {
+            val curHead = head.get()
+            val curTail = tail.get()
+            val next = curHead.next.get()
+            if (head.get() === curHead) {
+                if (curHead === curTail) {
+                    if (next == null) {
+                        return null
+                    }
+                    tail.compareAndSet(curTail, next)
+                } else {
+                    element = next?.element
+                    if (head.compareAndSet(curHead, next)) {
+                        head.get().element = null
+                        return element
+                    }
+                }
+            }
+        }
     }
 
     // FOR TEST PURPOSE, DO NOT CHANGE IT.
